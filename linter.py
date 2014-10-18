ebb9b17bca4cdc1d3a7a9b797ee64421649ca31b
SirReal <jon.surrell@gmail.com>
2014-10-18 16:42:49 +0200

    Ignore errors reported from imported files
    Fixes #3

--- linter.py
+++ linter.py
@@ -11,15 +11,29 @@
 """This module exports the Ghc plugin class."""
 
 from SublimeLinter.lint import Linter, util
-
+from os.path import basename
 
 class Ghc(Linter):
 
     """Provides an interface to ghc."""
 
+    def split_match(self, match):
+        """
+        Override to ignore errors reported in imported files.
+        """
+        match, line, col, error, warning, message, near = super().split_match(match)
+
+        match_filename = basename(match.groupdict()['filename'])
+        linted_filename = basename(self.filename)
+
+        if match_filename != linted_filename:
+            return None, None, None, None, None, '', None
+
+        return match, line, col, error, warning, message, near
+
     syntax = ('haskell', 'haskell-sublimehaskell', 'literate haskell')
     cmd = ('ghc', '-fno-code', '-Wall', '-Wwarn', '-fno-helpful-errors')
-    regex = r'^.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<warning>Warning:\s+)?(?P<message>.+)$'
+    regex = r'^(?P<filename>.+):(?P<line>\d+):(?P<col>\d+):\s+(?P<warning>Warning:\s+)?(?P<message>.+)$'
     multiline = True
 
     # No stdin
