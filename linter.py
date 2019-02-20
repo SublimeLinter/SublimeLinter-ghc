44291f8c9756716f81380c34d0d52b96a0958e2a
Neil Mayhew <neil@neil.mayhew.name>
2019-02-20 14:22:36 -0700

    Fix logic for ignoring errors in imported files
    Due to the use of a temporary filename, all errors were being ignored.

--- linter.py
+++ linter.py
@@ -25,6 +25,11 @@ class Ghc(Linter):
     # ghc writes errors to STDERR
     error_stream = util.STREAM_STDERR
 
+    def finalize_cmd(self, cmd, context, **kwargs):
+        """Override to capture temporary filename."""
+        self.temp_file = context['temp_file']
+        return super().finalize_cmd(cmd, context, **kwargs)
+
     def split_match(self, match):
         """Override to ignore errors reported in imported files."""
         match, line, col, error, warning, message, near = (
@@ -32,7 +37,7 @@ class Ghc(Linter):
         )
 
         match_filename = basename(match.groupdict()['filename'])
-        linted_filename = basename(self.filename)
+        linted_filename = basename(self.temp_file)
 
         if match_filename != linted_filename:
             return None, None, None, None, None, '', None
