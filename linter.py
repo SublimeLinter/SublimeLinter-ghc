e8de59a563342d23ed5675d60671cb2840e75d3c
herr kaste <herr.kaste@gmail.com>
2022-09-02 23:28:17 +0200

    Do not override `finalize_cmd` just use `self.context`

--- linter.py
+++ linter.py
@@ -27,11 +27,6 @@ class Ghc(Linter):
     # ghc writes errors to STDERR
     error_stream = util.STREAM_STDERR
 
-    def finalize_cmd(self, cmd, context, **kwargs):
-        """Override to capture temporary filename."""
-        self.temp_file = context['temp_file']
-        return super().finalize_cmd(cmd, context, **kwargs)
-
     def split_match(self, match):
         """Override to ignore errors reported in imported files."""
         match, line, col, error, warning, message, near = (
@@ -39,7 +34,7 @@ class Ghc(Linter):
         )
 
         match_filename = basename(match.groupdict()['filename'])
-        linted_filename = basename(self.temp_file)
+        linted_filename = basename(self.context.get('temp_file'))
 
         if match_filename != linted_filename:
             return None, None, None, None, None, '', None
